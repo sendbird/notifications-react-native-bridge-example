fb20225d156e59dec364f408805b1a0f48d6674b
Alan Choi <alan.choi@sendbird.com>
2023-05-30 19:22:31 -0700

    migrate AppDelegate to Swift

---
+++ ios/RNSBNotifications/AppDelegate.swift
@@ -0,0 +1,23 @@
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+  var bridge: RCTBridge!
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    let jsCodeLocation: URL
+
+    jsCodeLocation = RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    let rootView = RCTRootView(bundleURL: jsCodeLocation, moduleName: "RNSBNotifications", initialProperties: nil, launchOptions: launchOptions)
+    let rootViewController = UIViewController()
+    rootViewController.view = rootView
+
+    self.window = UIWindow(frame: UIScreen.main.bounds)
+    self.window?.rootViewController = rootViewController
+    self.window?.makeKeyAndVisible()
+
+    return true
+  }
+}
